89d309ba0aa6a6ea21facb6777186c0cae064ea8
Christos Laggouras <savi@e-food.gr>
2022-03-29 16:24:25 +0300

    - Update the templateViewController.xib in order to supprot a view controller with navigation item

--- template/templateModule.swift
+++ template/templateModule.swift
@@ -3,7 +3,7 @@
 //  project_name
 //
 //  Created by user_name on dd/mm/yy.
-//  Copyright © yy user_name. All rights reserved.
+//  Copyright © yy project_name. All rights reserved.
 //
 
 class templateModule {
